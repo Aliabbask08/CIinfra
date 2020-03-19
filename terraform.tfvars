0122bc9541861990b2b7c6127cff76a888b58473
Aliabbask08 <bloglearn72@gmail.com>
2020-03-19 20:12:55 +0530

    initial

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+profile="default"
+region="ap-south-1"
+cidr="10.0.0.0/16"
+vpc_name="static"
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "profile" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+}
+
